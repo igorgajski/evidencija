419c0af34c6491e1fae758d9a1da55e8eafbe2fa
Igor Gajski <igor.gajski.vvg@gmail.com>
2021-01-20 20:41:42 +0100

    Views

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -25,7 +25,7 @@ class CountryController extends Controller
      */
     public function create()
     {
-        //
+        return view('countries.create');
     }
 
     /**
@@ -36,7 +36,12 @@ class CountryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+           'name' => 'required|unique:countries|max:150',
+           'native_name' => 'required|unique:countries|max:150',
+       ]);
+        $country = Country::create($validated);
+        return view('countries.show', compact('country'));
     }
 
     /**
@@ -47,7 +52,8 @@ class CountryController extends Controller
      */
     public function show($id)
     {
-        //
+        $country = Country::findOrFail($id);
+        return view('countries.show', compact('country'));
     }
 
     /**
@@ -58,7 +64,8 @@ class CountryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $country = Country::findOrFail($id);
+        return view('countries.edit', compact('country'));
     }
 
     /**
@@ -70,7 +77,16 @@ class CountryController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'native_name' => 'required|max:255',
+        ]);
+
+        $country = Country::findOrFail($id);
+        $country->fill($validated);
+        $country->save();
+
+        return view('countries.show', compact('country'));
     }
 
     /**
@@ -81,6 +97,7 @@ class CountryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Country::destroy($id);
+        return redirect()->route('countries.index');
     }
 }
